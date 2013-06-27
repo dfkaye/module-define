7d4f859212367bcc46a3edba902e343655c8edbc
david kaye <dfkaye@yahoo.com>
2013-06-27 15:27:02 -0700

    browser shim with tests, testem, travis, testling

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 if (process) {
     console.log('node');
     require('./node/module-define');
-} else if (window) {
-    console.log('window');
+} else if (window && document) {
+    console.log('window && document');
+    var script = document.createElement('script');
+    script.src = './browser/module-define.js';
+    document.getElementsByTagName('head')[0].appendChild(script);
 }
